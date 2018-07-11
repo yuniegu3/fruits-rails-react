b144c5d33de4be15a73b763f0b9ffcf484ecf89c
Yoon Jo Han <yuniegu3@gmail.com>
2018-07-11 16:14:52 -0400

    corrected the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do 
-	root to: 'site#index'
+	root to: 'home#index'
+
   	namespace :api do 
     	namespace :v1 do 
      		resources :fruits, only: [:index, :create, :destroy, :update]
