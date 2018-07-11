d518b1b3b98bb069d971d0cbb0035e23417f3db5
Yoon Jo Han <yuniegu3@gmail.com>
2018-07-11 16:06:59 -0400

    added root route, added home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do 
-  namespace :api do 
-    namespace :v1 do 
-     resources :fruits, only: [:index, :create, :destroy, :update]
+	root to: 'site#index'
+  	namespace :api do 
+    	namespace :v1 do 
+     		resources :fruits, only: [:index, :create, :destroy, :update]
     end 
   end 
 end
