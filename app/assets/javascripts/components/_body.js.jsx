1623049587ee549c09618dff9bdeeffbc86208b4
Yoon Jo Han <yuniegu3@gmail.com>
2018-08-06 19:36:34 -0400

    added in submit functions

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -4,7 +4,13 @@ constructor(props) {
     this.state = {
       fruits: []
     };
+      this.handleFormSubmit = this.handleFormSubmit.bind(this)
   }
+  
+handleFormSubmit(name, description){
+    console.log(name, description)
+}
+
 componentDidMount(){
     fetch('/api/v1/fruits.json')
       .then((response) => {return response.json()})
@@ -13,7 +19,7 @@ componentDidMount(){
 render(){
     return(
       <div>
-        <NewFruit />
+        <NewFruit handleFormSubmit={this.handleFormSubmit}/>
         <AllFruits fruits={this.state.fruits} />
       </div>
     )
