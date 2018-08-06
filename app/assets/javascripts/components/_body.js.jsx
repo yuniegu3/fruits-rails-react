42fab61b79a249a6f02baae2deb10dc082236981
Yoon Jo Han <yuniegu3@gmail.com>
2018-08-06 19:40:51 -0400

    got the submit to work, realized i didnt do anything with the fruits api, it is correctly making post request now and showing up accordingly

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -4,12 +4,30 @@ constructor(props) {
     this.state = {
       fruits: []
     };
-      this.handleFormSubmit = this.handleFormSubmit.bind(this)
+    this.handleFormSubmit = this.handleFormSubmit.bind(this)
+    this.addNewFruit = this.addNewFruit.bind(this)
   }
-  
-handleFormSubmit(name, description){
-    console.log(name, description)
-}
+
+  handleFormSubmit(name, description){
+    let body = JSON.stringify({fruit: {name: name, description:   description} })
+
+      fetch('http://localhost:3000/api/v1/fruits', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: body,
+    }).then((response) => {return response.json()})
+    .then((fruit)=>{
+      this.addNewFruit(fruit)
+    })
+    
+  }
+    addNewFruit(fruit){
+    this.setState({
+      fruits: this.state.fruits.concat(fruit)
+    })
+  }
 
 componentDidMount(){
     fetch('/api/v1/fruits.json')
